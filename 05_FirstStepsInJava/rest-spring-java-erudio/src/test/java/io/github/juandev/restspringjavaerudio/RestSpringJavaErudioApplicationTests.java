31704c13de6580fb06d004e91bbfc85e84b07ad4
Juan <juancmendonca@gmail.com>
2025-02-26 15:49:19 -0300

    First steps in java with spring-boot

---
+++ 05_FirstStepsInJava/rest-spring-java-erudio/src/test/java/io/github/juandev/restspringjavaerudio/RestSpringJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.juandev.restspringjavaerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringJavaErudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
